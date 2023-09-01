016dde956dbf6159cdebda1b57c259e0722ef84f
Kirk Lin <linkirk@163.com>
2023-09-02 02:35:40 +0800

    feat: add `unocss-preset-chinese` `unocss-preset-ease`

--- uno.config.ts
+++ uno.config.ts
@@ -8,6 +8,8 @@ import {
   transformerDirectives,
   transformerVariantGroup,
 } from "unocss";
+import presetChinese from "unocss-preset-chinese";
+import presetEase from "unocss-preset-ease";
 
 export default defineConfig({
   shortcuts: [
@@ -17,6 +19,8 @@ export default defineConfig({
   presets: [
     presetUno(),
     presetAttributify(),
+    presetChinese(),
+    presetEase(),
     presetIcons({
       scale: 1.2,
     }),
